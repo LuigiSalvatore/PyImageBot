d1f11422aa274987b639fe7eed5b6fb8a457d676
LuigiSalvatore <134612508+LuigiSalvatore@users.noreply.github.com>
2023-09-05 19:00:52 -0700

    more bugs

--- bot.py
+++ bot.py
@@ -12,13 +12,13 @@ INTERVAL = 5
 Wizard = 'a'
 Hero = 'u'
 Sniper = 'z'
-MonkePos = [(0,0), (0,0), (0,0), (0,0)]
-Upgrade1 = ','
-Upgrade2 = '.'
-Upgrade3 = '/'
+Upgrade1 = '1'
+Upgrade2 = '2'
+Upgrade3 = '3'
 
 def click(x,y):
     win32api.SetCursorPos((x,y))
+    time.sleep(0.2)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     time.sleep(0.02)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
@@ -27,36 +27,29 @@ def click(x,y):
 def waitTime(timeS):
     for (i) in range(timeS):
         if pyautogui.locateOnScreen('level_up.png', confidence= 0.9) != None:
-            click(90,500)
-            time.sleep(0.8)
-            click(92,512)
+            var = pyautogui.locateOnScreen('level_up.png', confidence= 0.9)
+            var = pyautogui.center(var)
+            click(var.x,var.y)
+            time.sleep(1)
+            click(var.x,var.y)
         time.sleep(1.0)
 
 def placeMonke(x,y,key):
     keyboard.press_and_release(key)
+    time.sleep(0.5)
     click(x,y)
-    i = 0
-    for (i) in range(3):
-        if MonkePos[i] == (0,0):
-            MonkePos[i] = (x,y)
-            break
 
-def upgrade(numUpgrade, Monkey):
-    match Monkey:
-        case 1:
-            var = MonkePos[0]
-        case 2:
-            var = MonkePos[1]
-        case 3:
-            var = MonkePos[2]
-        case _:
-            print("No Monkey!")
-            var = (0,0)
-    click(var) # clicks on the equivalent monkey's position
+def upgrade(numUpgrade, x, y):
+    
+    click(x,y) # clicks on the equivalent monkey's position
+    time.sleep(1)
+    win32api.keybd_event(keyboard.press_and_release)
+    # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    # time.sleep(0.02)
+    # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    pyautogui.press(numUpgrade)
     time.sleep(0.5)
-    keyboard.press_and_release(numUpgrade)
-    time.sleep(0.5)
-    keyboard.press_and_release(escape)
+    keyboard.press_and_release('esc')
 
 def waitForEnd():
     while True:
@@ -114,8 +107,8 @@ def initGame(StageName, Difficulty):
         exit
 
 while Games_played < Games_To_Play:
-    initGame("stage.png", "easy.png")
-    time.sleep(10)                  # Time for the stage to load
+    #initGame("stage.png", "easy.png")
+    time.sleep(3)                  # Time for the stage to load
     placeMonke(975, 439, Hero)      # Placed Hero (Numeber 1)
 
     #############################
@@ -127,25 +120,27 @@ while Games_played < Games_To_Play:
     
     #       Starts Round        #
     #############################
-    waitTime(40)                    # Waits 40 seconds
-    placeMonke(1000, 215, Sniper)   # Places Sniper (Numeber 2)
-    waitTime(17)                    # Waits 17 seconds
-    placeMonke(776, 516, Wizard)    # Places Wizard (Numeber 3)
-    upgrade(Upgrade2 , 3)           # Upgrades the second skill of Wizard
-    upgrade(Upgrade3 , 3)           # Upgrades the third skill of Wizard
-    waitTime(18)                    # Waits 18 seconds
-    upgrade(Upgrade3 , 3)           # Upgrades the third skill of Wizard
-    waitTime(60)                    # Waits 60 seconds
-    upgrade(Upgrade2 , 3)           # Upgrades the second skill of Wizard
-    waitTime(33)                    # Waits 33 seconds
-    upgrade(Upgrade3 , 3)           # Upgrades the third skill of Wizard
-    waitTime(16)                    # Waits 16 seconds
-    upgrade(Upgrade1 , 2)           # Upgrades the first skill of sniper
-    waitTime(2)                     # Waits 2 seconds
-    upgrade(Upgrade1 , 2)           # Upgrades the first skill of sniper
-    waitTime(5)                     # Waits 5 seconds
-    upgrade(Upgrade3 , 2)           # Upgrades the third skill of sniper
-    waitTime(40)                    # Waits 40 seconds
-    upgrade(Upgrade3 , 3)           # Upgrades the third skill of Wizard
-    waitForEnd()                    # Waits until the game is finished
+    waitTime(40)                                    # Waits 40 seconds
+    placeMonke(1000, 215, Sniper)                   # Places Sniper (Numeber 2)
+    waitTime(17)                                    # Waits 17 seconds
+    placeMonke(776, 516, Wizard)                    # Places Wizard (Numeber 3)
+    Wizard = [776, 516]
+    Sniper = [1000, 215]
+    upgrade(Upgrade2 , Wizard[0], Wizard[1])          # Upgrades the second skill of Wizard
+    upgrade(Upgrade3 , Wizard[0], Wizard[1])          # Upgrades the third skill of Wizard
+    waitTime(18)                                    # Waits 18 seconds
+    upgrade(Upgrade3 , Wizard[0], Wizard[1])          # Upgrades the third skill of Wizard
+    waitTime(60)                                    # Waits 60 seconds
+    upgrade(Upgrade2 , Wizard[0], Wizard[1])          # Upgrades the second skill of Wizard
+    waitTime(33)                                    # Waits 33 seconds
+    upgrade(Upgrade3 , Wizard[0], Wizard[1])          # Upgrades the third skill of Wizard
+    waitTime(16)                                    # Waits 16 seconds
+    upgrade(Upgrade1 , Sniper[0], Sniper[1])          # Upgrades the first skill of sniper
+    waitTime(2)                                     # Waits 2 seconds
+    upgrade(Upgrade1 , Sniper[0], Sniper[1])          # Upgrades the first skill of sniper
+    waitTime(5)                                     # Waits 5 seconds
+    upgrade(Upgrade3 , Sniper[0], Sniper[1])          # Upgrades the third skill of sniper
+    waitTime(40)                                    # Waits 40 seconds
+    upgrade(Upgrade3 , Wizard[0], Wizard[1])          # Upgrades the third skill of Wizard
+    waitForEnd()                                    # Waits until the game is finished
     Games_played = Games_played + 1
